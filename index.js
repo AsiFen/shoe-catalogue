9e850c72a24663b37ae3bb5704d4d8a297e6fcd8
Asisipho <missfente@gmail.com>
2023-11-02 08:22:25 +0200

    edited name

--- index.js
+++ index.js
@@ -37,16 +37,16 @@ app.use(express.static('public'))
 
 router.get("/", async (req, res) => {
 
-    const api_all_shoes = "https://shoes-api-rm9c.onrender.com/api/shoes";
+    const api_all_shoes = "http://localhost:9999/api/shoes";
     const shoes = (await axios.get(api_all_shoes)).data;
 
-    const api = "https://shoes-api-rm9c.onrender.com/api/shoes/brandnames";
+    const api = "http://localhost:9999/api/shoes/brandnames";
     const brandNames = (await axios.get(api)).data;
 
-    const api_sizes = "https://shoes-api-rm9c.onrender.com/api/shoes/sizes";
+    const api_sizes = "http://localhost:9999/api/shoes/sizes";
     const sizes = (await axios.get(api_sizes)).data;
 
-    const api_colors = "https://shoes-api-rm9c.onrender.com/api/shoes/colors";
+    const api_colors = "http://localhost:9999/api/shoes/colors";
     const colors = (await axios.get(api_colors)).data;
 
     res.render("index", {
@@ -59,15 +59,15 @@ router.get("/", async (req, res) => {
 
 router.get('/brand/:brand', async (req, res) => {
     let brand = req.params.brand;
-    const api_brand = `https://shoes-api-rm9c.onrender.com/api/shoes/brand/${brand}`;
+    const api_brand = `http://localhost:9999/api/shoes/brand/${brand}`;
     const shoes = (await axios.get(api_brand)).data;
-    const api_brandnames = "https://shoes-api-rm9c.onrender.com/api/shoes/brandnames";
+    const api_brandnames = "http://localhost:9999/api/shoes/brandnames";
     const brandNames = (await axios.get(api_brandnames)).data;
 
-    const api_sizes = "https://shoes-api-rm9c.onrender.com/api/shoes/sizes";
+    const api_sizes = "http://localhost:9999/api/shoes/sizes";
     const sizes = (await axios.get(api_sizes)).data;
 
-    const api_colors = "https://shoes-api-rm9c.onrender.com/api/shoes/colors";
+    const api_colors = "http://localhost:9999/api/shoes/colors";
     const colors = (await axios.get(api_colors)).data;
 
     res.render("index", {
@@ -80,17 +80,17 @@ router.get('/brand/:brand', async (req, res) => {
 
 router.get('/size/:size', async (req, res) => {
     let size_ = req.params.size;
-    const api_size = `https://shoes-api-rm9c.onrender.com/api/shoes/size/${size_}`;
+    const api_size = `http://localhost:9999/api/shoes/size/${size_}`;
 
     const shoes = (await axios.get(api_size)).data;
 
-    const api_brandnames = "https://shoes-api-rm9c.onrender.com/api/shoes/brandnames";
+    const api_brandnames = "http://localhost:9999/api/shoes/brandnames";
     const brandNames = (await axios.get(api_brandnames)).data;
 
-    const api_sizes = "https://shoes-api-rm9c.onrender.com/api/shoes/sizes";
+    const api_sizes = "http://localhost:9999/api/shoes/sizes";
     const sizes = (await axios.get(api_sizes)).data;
 
-    const api_colors = "https://shoes-api-rm9c.onrender.com/api/shoes/colors";
+    const api_colors = "http://localhost:9999/api/shoes/colors";
     const colors = (await axios.get(api_colors)).data;
 
     res.render("index", {
@@ -105,16 +105,16 @@ router.get('/size/:size', async (req, res) => {
 router.get('/color/:color', async (req, res) => {
 
     let color_params = req.params.color;
-    const api_size = `https://shoes-api-rm9c.onrender.com/api/shoes/colors/${color_params}`;
+    const api_size = `http://localhost:9999/api/shoes/colors/${color_params}`;
     const shoes = (await axios.get(api_size)).data;
 
-    const api_brandnames = "https://shoes-api-rm9c.onrender.com/api/shoes/brandnames";
+    const api_brandnames = "http://localhost:9999/api/shoes/brandnames";
     const brandNames = (await axios.get(api_brandnames)).data;
 
-    const api_sizes = "https://shoes-api-rm9c.onrender.com/api/shoes/sizes";
+    const api_sizes = "http://localhost:9999/api/shoes/sizes";
     const sizes = (await axios.get(api_sizes)).data;
 
-    const api_colors = "https://shoes-api-rm9c.onrender.com/api/shoes/colors";
+    const api_colors = "http://localhost:9999/api/shoes/colors";
     const colors = (await axios.get(api_colors)).data;
 
     res.render("index", {
@@ -134,16 +134,16 @@ router.post('/filter', async (req, res) => {
     let size_params = req.body.size;
     let brand_body = req.body.brand;
 
-    const api_brand_size = `https://shoes-api-rm9c.onrender.com/api/shoes/brand/${brand_body}/size/${size_params}`;
+    const api_brand_size = `http://localhost:9999/api/shoes/brand/${brand_body}/size/${size_params}`;
     const shoes = (await axios.get(api_brand_size)).data;
 
-    const api = "https://shoes-api-rm9c.onrender.com/api/shoes/brandnames";
+    const api = "http://localhost:9999/api/shoes/brandnames";
     const brandNames = (await axios.get(api)).data;
 
-    const api_sizes = "https://shoes-api-rm9c.onrender.com/api/shoes/sizes";
+    const api_sizes = "http://localhost:9999/api/shoes/sizes";
     const sizes = (await axios.get(api_sizes)).data;
 
-    const api_colors = "https://shoes-api-rm9c.onrender.com/api/shoes/colors";
+    const api_colors = "http://localhost:9999/api/shoes/colors";
     const colors = (await axios.get(api_colors)).data;
 
     res.render("index", {
@@ -155,7 +155,7 @@ router.post('/filter', async (req, res) => {
     })
 })
 
-router.get('/add-shoes', async (req, res) => {
+router.get('/add-shoe', async (req, res) => {
 
     res.render("addShoe", {
         size: 'hi'
@@ -165,7 +165,7 @@ let cartShoes = {};
 
 router.post('/addToCart/:id', async (req, res) => {
     const shoeId = req.params.id;
-    const addShoeId = `https://shoes-api-rm9c.onrender.com/api/shoes/${shoeId}`;
+    const addShoeId = `http://localhost:9999/api/shoes/${shoeId}`;
     cartShoes = (await axios.get(addShoeId)).data;
 
     console.log(cartShoes);
@@ -178,7 +178,7 @@ router.post('/addToCart/:id', async (req, res) => {
 });
 
 router.get('/cart', async (req, res) => {
-    const getCart = 'https://shoes-api-rm9c.onrender.com/api/shoes/cart';
+    const getCart = 'http://localhost:9999/api/shoes/cart';
     cartShoes = (await axios.get(getCart)).data;
 
     console.log(cartShoes);
@@ -193,7 +193,7 @@ router.get('/cart', async (req, res) => {
     }
 });
 
-router.post('/add-shoes', async (req, res) => {
+router.post('/add-shoe', async (req, res) => {
 
     let obj = {
         'color': req.body.color,
@@ -205,7 +205,7 @@ router.post('/add-shoes', async (req, res) => {
         'brand': req.body.brand
     }
     console.log(obj);
-    axios.post('https://shoes-api-rm9c.onrender.com/api/shoes', obj)
+    axios.post('http://localhost:9999/api/shoes', obj)
         .then(response => {
             // Handle the response from the API.
             if (response.status === 200) {
